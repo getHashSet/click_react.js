576ac78d65351fff12be7e44c24951c077bcd71f
Matthew <matthew_carpenter@icloud.com>
2019-12-14 19:45:35 -0700

    updated app to run using basic react framework.

--- click_app/src/App.js
+++ click_app/src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Index from "../public";
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  document.title = "Clicky App"
   return (
     <div className="App">
-      <Router>
-        <Route exact path="/" component={Index} />
-        <Route path="*" component={ErrorPage} />
-      </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
